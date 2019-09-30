4f8545a8aa59e75d8e4f86a7e77cc88089a0710c
unknown <MonLe399@ltpemdomain.local>
2019-09-30 09:08:10 +0200

    first

---
+++ Homework 1/js/extra.js
@@ -0,0 +1,11 @@
+var name = prompt("Greetings user, please enter your name.");
+var surName = prompt("Please enter your surname.");
+var age = prompt("Now, please enter your age.");
+
+if (age == 17) { ori + yoAdult };
+
+if (age >= 18) { ori + adult };
+
+var ori = alert("Welcome " + name + " " + surName + "! I see that next year you will be " + (Number(age) + 1) + " years old. ");
+var yoAdult = alert("years old. You will become an adult!");
+var adult = alert("years old. How's the adult life going?");
