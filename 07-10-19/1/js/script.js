b320cab2c2141d354bcc1ae2b89ac6d4310ea7b8
unknown <MonLe399@ltpemdomain.local>
2019-10-07 09:50:19 +0200

    1

---
+++ 07-10-19/1/js/script.js
@@ -0,0 +1,12 @@
+name1 = "John";
+name2 = "Doe";
+
+console.log(name1 + " " + name2)
+
+x = name1;
+
+name1 = name2;
+
+name2 = x;
+
+console.log(name1 + " " + name2);
---
+++ 07-10-19/2 - fibonacci/js/script.js
@@ -0,0 +1,4 @@
+var fib = 1;
+var fib2 = 1;
+
+var fib2 = fib + fib2;
