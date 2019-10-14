d05df1176af2ef9bc0e816af64e864aa332ea68c
unknown <MonLe399@ltpemdomain.local>
2019-10-14 09:50:56 +0200

    1

---
+++ 14-10-19/js/script.js
@@ -0,0 +1,38 @@
+// var i = 0;
+// var sum = 0;
+// var n = 10;
+
+// console.log("The value of n is " + n)
+// console.log("---------------------------------------");
+
+// for (i; i <= n; i++) {
+
+//     sum = sum + i;
+//     console.log(sum);
+// }
+
+
+
+//-----------------------------------------------------------------------------------------------------
+
+var a = 1;
+var b = 1;
+
+console.log(a, b);
+
+for (c = 0; c <= 100; c++) {
+    // var c = a + b;
+
+    // a = b;
+
+    // b = c;
+
+    // console.log(c);
+
+    for (i = 0; i <= 100; i++) {
+        console.log(i);
+    }
+
+}
+var n = c.toString();
+document.getElementById("id").innerHTML = n;
