6262e3559ace036534e6efd815ff49a7a1fa8d88
unknown <MonLe399@ltpemdomain.local>
2019-10-21 09:49:48 +0200

    first

---
+++ 21-10-19/js/script3.js
@@ -0,0 +1,11 @@
+function Start() {
+    document.getElementById("myTable");
+
+    for (var r = 0; r !== 5; r++) {
+        var tbl = "<tr>";
+        for (var c = 0; c !== 8; c++) {
+
+        }
+        tbl += "</tr>";
+    }
+}
