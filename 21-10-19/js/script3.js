cc76194bbcad6b3384824af79f485c92b151119d
Sorulm <leonardomonticelli19@gmail.com>
2019-10-21 10:33:03 +0200

    a

--- 21-10-19/js/script3.js
+++ 21-10-19/js/script3.js
@@ -4,7 +4,8 @@ function Start() {
     for (var r = 0; r !== 5; r++) {
         var tbl = "<tr>";
         for (var c = 0; c !== 8; c++) {
-
+            var u = r * c;
+            var cell = "<td>" + u.toString + "</td>";
         }
         tbl += "</tr>";
     }
