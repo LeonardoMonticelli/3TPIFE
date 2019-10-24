70ac97ab5f043d16671eb18a59a7c437755f1842
Sorulm <leonardomonticelli19@gmail.com>
2019-10-24 11:49:59 +0200

    1

---
+++ Test 1 24-10-19/js/leonardoMonticelli2.js
@@ -0,0 +1,23 @@
+//Exercise 2
+//a
+
+console.log("a.");
+
+var n = 5; //n>0
+var nat = 1 + (1 / n);
+console.log("(1 + 1/" + n + ") is " + nat);
+
+console.log("---------------------");
+
+console.log("b.");
+
+var pow = 1;
+var n = 2 // exp
+
+var nat = 1 + (1 / n);
+
+for (i = 0; i > n; i++) {
+    pow = pow * nat;
+}
+
+console.log("(1 + 1/" + n + ") raised to the power of " + n + " is " + nat);
