03bd8cb9aac35172eae9a1c681246eca09192dae
Sorulm <leonardomonticelli19@gmail.com>
2019-11-04 11:39:46 +0100

    2

--- Extra_Ex for Test1/NewEx/Ex16/js/script.js
+++ Extra_Ex for Test1/NewEx/Ex16/js/script.js
@@ -1,15 +1,65 @@
 //S1
-var promptS1 = prompt("please enter the value of n");
-var n = Number(promptS1);
+// var promptS1 = prompt("please enter the value of n");
+// var n = Number(promptS1);
+
+// var sum = 0;
+// var int = 1;
+
+// for (var i = int; i <= n; i++) {
+//     sum += i
+//     console.log(i);
+// }
+// console.log("The sum of the numbers from " + int + " to " + n + " is = " + sum);
+
+//S2
+// var promptS2 = prompt("please enter the value of n for S2");
+// var n = Number(promptS2);
+
+// var sum = 0;
+// var int = 1;
+
+// for (var i = int; i <= n; i++) {
+//     sum += (i * i)
+//     console.log("value of i= " + i);
+// }
+// console.log("The sum of the numbers from " + int + " to " + n + " is = " + sum);
+
+// //S3
+// var promptS3 = prompt("please enter the value of n for S3");
+// var n = Number(promptS3);
+
+// var sum = 0;
+// var int = 1;
+
+// for (var i = int; i <= n; i++) {
+//     sum += 3 * i * (i - 1)
+//     console.log("value of i= " + i);
+// }
+// console.log("The sum of the numbers from " + int + " to " + n + " is = " + sum);
+
+//S4
+var promptS4 = prompt("please enter the value of n for S4");
+var n = Number(promptS4);
 
 var sum = 0;
 var int = 1;
+var i = int;
 
-for (var i = int; i <= n; i++) {
-    sum += i
-    console.log(i);
+var mint = 2;
+
+var pow = 1;
+
+for (var another = 1; another <= i; another++) {
+    pow = pow * mint;
+    console.log("The product of " + mint + " to the power of i= " + i + " is " + pow)
 }
-console.log("The sum of the numbers from " + int + " to " + n + " is = " + sum)
+
+for (i; i <= n; i++) {
+    sum += (1 / pow)
+        //console.log("value of i= " + i);
+}
+console.log("The sum of the numbers from " + int + " to " + n + " is = " + sum);
+
 
 // var Pm = prompt("give me m");
 // var Pn = prompt("give me n");
--- Extra_Ex for Test1/NewEx/Ex17/js/script.js
+++ Extra_Ex for Test1/NewEx/Ex17/js/script.js
@@ -1,8 +1,11 @@
 var promptN = prompt("please enter the value of n");
 var n = Number(promptN);
 
-var powK = 1;
+var m = -1;
+var pow = 1;
+console.log("being that n is = " + n);
 
-for (k = 1; k <= n; k++) {
-    powK += k;
+for (i = 1; i <= n; i++) {
+    pow = pow * m;
+    console.log("i is= " + i + "| (-1) to the power of i is = " + pow);
 }
