49e4cee9581d92883e438d5bff4c418c4a8c02d8
unknown <MonLe399@ltpemdomain.local>
2019-12-02 09:50:10 +0100

    1

---
+++ Classes/12-02-19/js/script.js
@@ -0,0 +1,7 @@
+function button(x) {
+    // var number = document.getElementById()
+    alert("you clicked on " + x)
+}
+
+// var result = "";
+// document.getElementById("result").innerHTML = result;
