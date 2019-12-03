ffaace31f77d9f8cfd58d05a9c75a8a250826a44
unknown <MonLe399@ltpemdomain.local>
2019-12-03 14:25:06 +0100

    1

--- Classes/12-02-19/js/script.js
+++ Classes/12-02-19/js/script.js
@@ -1,7 +1,37 @@
+var cache = 0;
+
 function button(x) {
-    // var number = document.getElementById()
-    alert("you clicked on " + x)
+    /*
+    TWO OPTIONS for finding out that the + was pressed OR not
+    1. Get the + button, check its classname and see if it contains the class turnOn
+    */
+    if (document.getElementById("plus").classList.contains("turnOn")) {
+
+        document.getElementById("plus").classList.remove("turnOn");
+        cache = document.getElementById("result").value
+        document.getElementById("result").value = "";
+    }
+    document.getElementById("result").value += x;
 }
 
-// var result = "";
-// document.getElementById("result").innerHTML = result;
+
+function reset() {
+    document.getElementById("result").value = null;
+    cache = 0;
+    document.getElementById("plus").classList.remove("turnOn")
+}
+
+function plus() {
+    var result = Number(document.getElementById("result").value)
+    result += Number(cache);
+    document.getElementById("result").value = result;
+    document.getElementById("plus").classList.add("turnOn");
+}
+
+function equal() {
+
+}
+
+// for (i = 0; i < 10; i -= -1) {
+//     console.log(i);
+// }
