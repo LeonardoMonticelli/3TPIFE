efd1782fc252d4f7b256840c514ea4ae00a262d0
unknown <MonLe399@ltpemdomain.local>
2019-12-09 09:50:05 +0100

    1

--- Classes/12-02-19/js/script.js
+++ Classes/12-02-19/js/script.js
@@ -1,37 +1,48 @@
-var cache = 0;
+var cache = null;
 
 function button(x) {
     /*
     TWO OPTIONS for finding out that the + was pressed OR not
     1. Get the + button, check its classname and see if it contains the class turnOn
     */
-    if (document.getElementById("plus").classList.contains("turnOn")) {
+    if (document.getElementById("plus").classList.contains("turnOn") ||
+        document.getElementById("minus").classList.contains("turnOn")) {
 
-        document.getElementById("plus").classList.remove("turnOn");
-        cache = document.getElementById("result").value
+        resetDisplay();
+        cache = document.getElementById("result").value;
         document.getElementById("result").value = "";
     }
     document.getElementById("result").value += x;
 }
 
+function resetDisplay() {
+    document.getElementById("plus").classList.remove("turnOn");
+    document.getElementById("minus").classList.remove("turnOn");
+}
 
 function reset() {
     document.getElementById("result").value = null;
-    cache = 0;
-    document.getElementById("plus").classList.remove("turnOn")
+    cache = null;
+
+    resetDisplay();
 }
 
 function plus() {
-    var result = Number(document.getElementById("result").value)
+    var result = Number(document.getElementById("result").value);
     result += Number(cache);
     document.getElementById("result").value = result;
-    document.getElementById("plus").classList.add("turnOn");
+
+    resetDisplay();
+    document.getElementById("plus").classList.add("turnOn");;
 }
 
-function equal() {
+function minus() {
+    if (cache != null) {
+        var result = Number(document.getElementById("result").value);
+        result = Number(cache) - result;
+        document.getElementById("result").value = result;
+    }
 
+    resetDisplay();
+    document.getElementById("minus").classList.add("turnOn");
 }
-
-// for (i = 0; i < 10; i -= -1) {
-//     console.log(i);
-// }
