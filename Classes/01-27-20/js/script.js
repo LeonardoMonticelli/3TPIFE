74265b178e38c2c9f375524ecb8c6ccc842bf4cb
unknown <MonLe399@ltpemdomain.local>
2020-01-28 14:25:04 +0100

    28.01

--- Classes/01-27-20/js/script.js
+++ Classes/01-27-20/js/script.js
@@ -1,15 +1,33 @@
-var area = 0;
-for (var x = -10; x < 11; x++) {
+// var area = 0;
+// var r = 1000;
+// for (var x = -r; x <= r; x++) {
+//     for (var y = -r; y <= r; y++) {
+//         var c = Math.sqrt((x * x) + (y * y)); //pitagoras theorem
+//         if (c <= r) { //lenght of the hipothenuse and the radius
+//             //console.log(x, y);
+//             area++;
+//         }
+//     }
+// }
+// var pi = area / (r * r)
 
-    for (var y = -10; y < 11; y++) {
-        var c = Math.sqrt((x * x) + (y * y));
+// console.log("area: " + area);
 
-        if (c <= 10) {
-            console.log(x, y);
-            area++;
-        }
-    }
+// var pi = 1 / 1 - 1 / 3 + 1 / 5 - 1 / 7 + 1 / 9;
+
+// Leibniz
+var n = 1;
+var pi = (1 / n);
+
+for (let a = 0; a <= 5; a++) {
+    n = n + 2;
+    pi = pi - (1 / n);
+    n = n + 2;
+    pi = pi + (1 / n);
+    n = n + 2;
+    pi = pi - (1 / n);
 }
-console.log("area: " + area);
-// console.log("lenght: " + c);
-// console.log("pi: " + pi);
+
+pi = pi * 4;
+
+console.log("pi: " + pi);
