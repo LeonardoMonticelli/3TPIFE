0405894ec069535014f209100eaa13873c943bd8
unknown <MonLe399@ltpemdomain.local>
2020-03-10 14:23:10 +0100

    10/03

--- Classes/Sem2/February 26th/js/script.js
+++ Classes/Sem2/February 26th/js/script.js
@@ -1,12 +1,32 @@
-function alertButton() {
-    // let genders = document.getElementsByName("gender"),
-    //     selected;
-    // genders.forEach((button) => { if (button.checked === true) selected = button.value; });
-    // alert(selected);
-
-    //
+ coloredBox();
 
 
-    let yourmom = document.querySelector('input[type="radio"]:checked+label');
-    alert(yourmom.innerHTML)
-}
+ function alertButton() {
+     // let genders = document.getElementsByName("gender"),
+     //     selected;
+     // genders.forEach((button) => { if (button.checked === true) selected = button.value; });
+     // alert(selected);
+     let yourmom = document.querySelector('input[type="radio"]:checked+label');
+     if (yourmom != null) {
+         alert(yourmom.innerHTML)
+     }
+
+     let element = document.getElementById("whatever");
+     element.style.backgroundColor = "pink"
+ }
+
+ function buildColor(r, g, b) {
+     let retVal = "rgb(" + r + "," + g + "," + b + ")";
+     return retVal;
+ }
+
+ function coloredBox() {
+     let color = document.getElementById("coloredBox");
+     let a = Math.floor(Math.random() * 255);
+     let b = Math.floor(Math.random() * 255);
+     let c = Math.floor(Math.random() * 255);
+     let stringColor = buildColor(a, b, c);
+     console.log(stringColor);
+
+     color.style.backgroundColor = stringColor;
+ }
